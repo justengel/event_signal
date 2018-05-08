d7ccedffde1d74491cb91f3a97d4ff1e121ddaad
jengel <jengel@sealandaire.com>
2018-05-08 16:52:00 -0400

    More setup.py updates.

--- setup.py
+++ setup.py
@@ -6,14 +6,8 @@ See Also:
     https://pythonhosted.org/an_example_pypi_project/setuptools.html
 """
 import os
-
+import glob
 from setuptools import setup
-
-
-def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
 
 
 def read(fname):
@@ -22,63 +16,81 @@ def read(fname):
         return file.read()
 
 
+# ========== Requirements ==========
+def check_options(line, options):
+    if line.startswith('--'):
+        opt, value = line.split(' ')
+        opt = opt.strip()
+        value = value.strip()
+        try:
+            options[opt].append(value)
+        except KeyError:
+            options[opt] = [value]
+        return True
+
+
+def parse_requirements(filename, options=None):
+    """load requirements from a pip requirements file """
+    if options is None:
+        options = {}
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#") and not check_options(line, options)]
+
+
+requirements = parse_requirements('requirements.txt')
+# ========== END Requirements ==========
+
+
 if __name__ == "__main__":
-    # ===== Requirements =====
-    try:
-        requirements = parse_requirements("requirements.txt")
-    except FileNotFoundError:
-        requirements = []
-    # ===== END Requirements =====
+    setup(name="event_signal",
+          version="1.5.2",
+          description="Library to help notify when something has changed.",
+          url="https://github.com/HashSplat/event_signal",
+          download_url="https://github.com/HashSplat/event_signal/archive/v1.5.2.tar.gz",
 
-    setup(
-        name="event_signal",
-        version="1.5.2",
-        description="Library to help notify when something has changed.",
-        url="https://github.com/HashSplat/event_signal",
-        download_url="https://github.com/HashSplat/event_signal/archive/v1.5.2.tar.gz",
+          keywords=["signal", "observer", "signaler", "event", "javascript events", "bind"],
 
-        keywords=["signal", "observer", "signaler", "event", "javascript events", "bind"],
+          author="Justin Engel",
+          author_email="jtengel08@gmail.com",
 
-        author="Justin Engel",
-        author_email="jtengel08@gmail.com",
+          license="MIT",
 
-        license="MIT",
+          platforms="any",
+          classifiers=["Programming Language :: Python",
+                       "Programming Language :: Python :: 3",
+                       "Operating System :: OS Independent"],
 
-        platforms="any",
-        classifiers=["Programming Language :: Python",
-                     "Programming Language :: Python :: 3",
-                     "Operating System :: OS Independent"],
+          scripts=[file for file in glob.iglob("bin/*.py")],
 
-        scripts=[],
+          long_description=read("README.md"),
+          packages=["event_signal"],
 
-        long_description=read("README.md"),
-        packages=["event_signal"],
-        install_requires=requirements,
+          install_requires=requirements,
 
-        include_package_data=False,
+          include_package_data=False,
 
-        # package_data={
-        #     'package': ['file.dat']
-        # }
+          # package_data={
+          #     'package': ['file.dat']
+          # }
 
-        # options to install extra requirements
-        # extras_require={
-        #     'dev': [],
-        #     'test': ['converage'],
-        # }
+          # options to install extra requirements
+          # extras_require={
+          #     'dev': [],
+          #     'test': ['converage'],
+          # }
 
-        # Data files outside of packages
-        # data_files=[('my_data', ["data/my_data.dat"])],
+          # Data files outside of packages
+          # data_files=[('my_data', ['data/data_file'])],
 
-        # keywords='sample setuptools development'
+          # keywords='sample setuptools development'
 
-        # entry_points={
-        #     'console_scripts': [
-        #         'foo = my_package.some_module:main_func',
-        #         'bar = other_module:some_func',
-        #     ],
-        #     'gui_scripts': [
-        #         'baz = my_package_gui:start_func',
-        #     ]
-        # }
-    )
+          # entry_points={
+          #     'console_scripts': [
+          #         'foo = my_package.some_module:main_func',
+          #         'bar = other_module:some_func',
+          #     ],
+          #     'gui_scripts': [
+          #         'baz = my_package_gui:start_func',
+          #     ]
+          # }
+          )
